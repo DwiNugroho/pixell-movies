87fb26d47a998d0c7af4f07cd54a35092723110c
DwiNugroho <adwinugroho1@gmail.com>
2021-06-25 00:42:29 +0700

    chore: create sass architecture

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb-typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -12,9 +12,11 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
     ecmaVersion: 2020,
+    project: ['./tsconfig.json'],
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
@@ -28,5 +30,20 @@ module.exports = {
         ExportDeclaration: { multiline: false, minProperties: 3 },
       },
     ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          '@': './src/',
+          '@components': './src/components',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        packages: ['node_modules/*'],
+      },
+    },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,38 @@ import React from 'react';
 const App = () => (
   <section className="container">
     <h1>Hello World</h1>
+    <h2>Hello World</h2>
+    <h3>Hello World</h3>
+    <h4>Hello World</h4>
+    <h5>Hello World</h5>
+    <h6>Hello World</h6>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime deleniti
+      beatae quisquam rem repellat laboriosam, incidunt totam, natus corrupti
+      distinctio odit culpa, maiores illo eligendi unde dolore perferendis
+      officiis illum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iure
+      nostrum aperiam, minima architecto inventore cupiditate in, ratione sequi,
+      exercitationem ea atque? Repellendus ratione consequatur fugiat nam dolore
+      deserunt nisi?
+    </p>
+
+    <section className="flex flex--wrap">
+      <div
+        className="flex__item flex__item--12 flex__item--lg-6 background--red"
+        style={{
+          minHeight: '100px',
+        }}
+      />
+      <div
+        className="flex__item flex__item--12 flex__item--lg-6 background--red"
+        style={{
+          minHeight: '100px',
+        }}
+      />
+    </section>
   </section>
 );
 
